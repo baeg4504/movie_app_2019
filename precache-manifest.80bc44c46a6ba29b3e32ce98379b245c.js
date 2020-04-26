56ee6e1d13d90f43f84c735437b6e4cd0b471525
Seungchan Baek <baeg4504@gmail.com>
2020-04-27 00:59:08 +0900

    Updates

---
+++ precache-manifest.80bc44c46a6ba29b3e32ce98379b245c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db99057344a576b890bf9c00eb8fe4fd",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "abf745f1df36784d8c6b",
+    "url": "/movie_app_2019/static/css/main.f7fe12bf.chunk.css"
+  },
+  {
+    "revision": "c2b13cb1a43739ba71ce",
+    "url": "/movie_app_2019/static/js/2.f51afafd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.f51afafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf745f1df36784d8c6b",
+    "url": "/movie_app_2019/static/js/main.7fa59b96.chunk.js"
+  },
+  {
+    "revision": "ed13f68ad2334e5f99e5",
+    "url": "/movie_app_2019/static/js/runtime-main.49cba434.js"
+  }
+]);
